c671ccb1b61dccd7c9a3ed89c5f03431003d71d6
itzikbraun <itzik.braun@gmail.com>
2014-06-15 10:43:47 +0300

    Fix

---
+++ Main/src/main/java/braunster/babymonitor/activities/IncomingCallDataActivity.java
@@ -0,0 +1,35 @@
+package braunster.babymonitor.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by itzik on 5/19/2014.
+ */
+public class IncomingCallDataActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
